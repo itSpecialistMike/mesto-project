9087b5a67613022ab744c5646847e3174925e2fd
itSpecialistMike <personal.hardworkin@gmail.com>
2025-11-20 14:36:57 +0700

    feat/show likes

--- src/scripts/components/cards.js
+++ src/scripts/components/cards.js
@@ -1,32 +1,47 @@
-// // файл cards.js
 import {fetchCards, postCard} from "./fetchs";
 
 const placesList = document.querySelector(".places__list");
 
 // Темплейт карточки
-function cardTemplate(link, name) {
+function cardTemplate(link, name, likes) {
     const template = document.querySelector('#card-template');
     const cardElement = template.content.cloneNode(true);
     cardElement.querySelector('.card__image').src = link;
     cardElement.querySelector('.card__image').alt = name;
     cardElement.querySelector('.card__title').textContent = name;
+    cardElement.querySelector('.card__likes').textContent = likes.length;
     return cardElement;
 }
 
 // Функция создания карточки
 function createCard(name, link) {
-    postCard(name, link).then(() => renderCards());
+    console.log('Создание карточки:', { name, link });
+
+    postCard(name, link)
+        .then(card => {
+            console.log('Карточка создана успешно:', card);
+            placesList.prepend(cardTemplate(card.link, card.name));// prepend - новая карточка сверху
+            return card;
+        })
+        .catch(err => {
+            console.error('Ошибка создания карточки:', err);
+        });
 }
 
 // Вывести карточки на страницу
 function renderCards() {
     placesList.innerHTML = '';
-    fetchCards().then(cards => {
-        cards.forEach(card => {
-        placesList.append(cardTemplate(card.link, card.name));
-    });})
+    console.log('Начало загрузки карточек...');
+
+    fetchCards()
+        .then(cards => {
+            console.log(`Загружено ${cards.length} карточек`);
+            cards.forEach(card => {
+                placesList.append(cardTemplate(card.link, card.name, card.likes, card.owner, card._id));
+            });
+        })
         .catch(err => {
-            console.error('Ошибка загрузки профиля:', err);
+            console.error('Ошибка загрузки карточек:', err);
         });
 }
 
--- src/scripts/components/fetchs.js
+++ src/scripts/components/fetchs.js
@@ -3,12 +3,7 @@ const fetchCards = () => {
         headers: {
             authorization: `18e65fe6-f22e-4549-8c48-91a029fe4686`,
         }
-    }).then(res => {
-        if (res.ok) {
-            return res.json()
-        }
-        return Promise.reject(`Ошибка ${res.statusText}`)
-    })
+    }).then(catchError)
 }
 
 const postCard = (name, link) => {
@@ -19,7 +14,7 @@ const postCard = (name, link) => {
             'Content-Type': "application/json",
             authorization: `18e65fe6-f22e-4549-8c48-91a029fe4686`
         }
-    })
+    }).then(catchError)
 }
 
 const fetchUser = () => {
@@ -27,28 +22,29 @@ const fetchUser = () => {
         headers: {
             authorization: `18e65fe6-f22e-4549-8c48-91a029fe4686`,
         }
-    }).then(res => {
-        if (res.ok) {
-            return res.json()
-        }
-        return Promise.reject(`Ошибка ${res.statusText}`)
-    })
+    }).then(catchError)
 }
 
 const patchUser = (name, about) => {
-    return fetch('https://mesto.nomoreparties.co/v1/apf-cohort-202/users/me',
-        {
-            method: "PATCH",
-            headers: {
-                'Content-Type': 'application/json',
-                authorization: `18e65fe6-f22e-4549-8c48-91a029fe4686`,
-            },
-            body: JSON.stringify({
-                name: name,
-                about: about})
+    return fetch('https://mesto.nomoreparties.co/v1/apf-cohort-202/users/me', {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json',
+            authorization: `18e65fe6-f22e-4549-8c48-91a029fe4686`,
+        },
+        body: JSON.stringify({
+            name: name,
+            about: about
         })
+    }).then(catchError)
 }
 
+function catchError(res) {
+    if (res.ok) {
+        return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+}
 
 export {
     fetchCards,
